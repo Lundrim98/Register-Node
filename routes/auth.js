7c108b387a5cb1db30af3875ae8c12c6c537faf0
Lundrim <81919503+Lundrim98@users.noreply.github.com>
2021-12-20 15:47:12 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,11 @@ router.post('/register', async (req, res) => {
 const { error } = registerValidation(req.body);
 if (error) return res.status(400).send(error.details[0].message);
 
-    //Checking if the user is already in database
+    //Checking if the user is registered
     const emailExist = await User.findOne({ email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
-    //Hash  paswords
+    //Hashing the  passwords
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -33,7 +33,7 @@ if (error) return res.status(400).send(error.details[0].message);
 });
 //Login
 router.post('/login', async (req,res) => {
-    //Lets Validate the date before we make a user
+    //Validate the date before we make a user
     const { error } = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
         //Checking if the email exists

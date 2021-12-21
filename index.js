f585764306d689a806539a9c218ec0b8159401f5
Lundrim <81919503+Lundrim98@users.noreply.github.com>
2021-12-21 14:47:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 //Import Routes
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
+    const authRoute = require('./routes/auth');
+    const postRoute = require('./routes/posts');
 
-require('dotenv').config();
+    require('dotenv').config();
 
 //Connect to db
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
-console.log('connected to db!')
+    mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+    console.log('connected to db!')
 );
 
 //Middleware
-app.use(express.json());
+    app.use(express.json());
 //Route Middlewares
-app.use('/api/user', authRoute);
-app.use('/api/posts', postRoute);
+    app.use('/api/user', authRoute);
+    app.use('/api/posts', postRoute);
 
 app.listen(3000, () => console.log('Server is running'));

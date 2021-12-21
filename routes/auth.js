9ed3742fc4ff6a615f6143f460986c693a630ece
Lundrim <81919503+Lundrim98@users.noreply.github.com>
2021-12-21 14:46:02 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,19 +6,19 @@ const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
 // Validate the date before we make a user
-const { error } = registerValidation(req.body);
-if (error) return res.status(400).send(error.details[0].message);
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    //Checking if the user is registered
+//Checking if the user is registered
     const emailExist = await User.findOne({ email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
-    //Hashing the  passwords
+//Hashing the  passwords
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 
-    //Create a new user
+//Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -32,21 +32,21 @@ if (error) return res.status(400).send(error.details[0].message);
     }
 });
 //Login
-router.post('/login', async (req,res) => {
+    router.post('/login', async (req,res) => {
     
-    //Validate the date before we make a user
+//Validate the date before we make a user
         const { error } = loginValidation(req.body);
         if (error) return res.status(400).send(error.details[0].message);
     
-    //Checking if the email exists
+//Checking if the email exists
         const user = await User.findOne({ email: req.body.email});
         if(!user) return res.status(400).send('Email is not found');
     
-    //Password is correct
+//Password is correct
         const validPass = await bcrypt.compare(req.body.password, user.password);
         if(!validPass) return res.status(400).send('Invalid password');
 
-    //Create a token
+//Create a token
         const token = jwt.sign({ _id: user._id}, process.env.TOKEN_SECRET);
         res.header('auth-token', token).send(token);
 });

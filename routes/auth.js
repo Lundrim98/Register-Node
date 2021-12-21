aff2a3710bf002dd2cc699fde3574b8d1ad03604
Lundrim <81919503+Lundrim98@users.noreply.github.com>
2021-12-21 14:13:44 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,7 @@ router.post('/login', async (req,res) => {
 
     //Create a token
     const token = jwt.sign({ _id: user._id}, process.env.TOKEN_SECRET);
-    res.header('privat-token', token).send(token);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router;   

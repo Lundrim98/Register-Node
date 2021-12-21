4fc259c9ad0204bbd0c0ea26aa78cd863a5caaef
Lundrim <81919503+Lundrim98@users.noreply.github.com>
2021-12-21 14:18:20 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -33,19 +33,22 @@ if (error) return res.status(400).send(error.details[0].message);
 });
 //Login
 router.post('/login', async (req,res) => {
+    
     //Validate the date before we make a user
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-        //Checking if the email exists
-    const user = await User.findOne({ email: req.body.email});
-    if(!user) return res.status(400).send('Email is not found');
+        const { error } = loginValidation(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+    
+    //Checking if the email exists
+        const user = await User.findOne({ email: req.body.email});
+        if(!user) return res.status(400).send('Email is not found');
+    
     //Password is correct
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('Invalid password');
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if(!validPass) return res.status(400).send('Invalid password');
 
     //Create a token
-    const token = jwt.sign({ _id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token);
+        const token = jwt.sign({ _id: user._id}, process.env.TOKEN_SECRET);
+        res.header('auth-token', token).send(token);
 });
 
 module.exports = router;   
